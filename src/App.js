54bf527829b4a85e6178aa7917b10e1ebcd57178
ElenaTkacheva <79642915+ElenaTkacheva@users.noreply.github.com>
2022-01-31 13:50:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data";
+import "./App.css";
 
 function App() {
+
+  const [sights, setSights] = useState(0);
+  const { id, name, description, rating, image } = data[sights];
+  const [showMore, setShowMore] = useState(false);
+
+
+  const previousSight = () => {
+    setSights((sight) => {
+      sight--;
+      if (sight < 0) {
+        return data.length - 1;
+      }
+      return sight;
+    });
+  };
+
+  const nextSight = () => {
+    setSights((sight) => {
+      sight++;
+      if (sight > data.length - 1) {
+        sight = 0;
+      }
+      return sight;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main">
+        <div className="container">
+          <h1>Top {data.length} sights in Montreal</h1>
+        </div>
+        <div className="container">
+          <h2>
+            {id}. {name}
+          </h2>
+        </div>
+        <div className="container">
+          <img src={image} width="350px" alt="sight" />
+        </div>
+        <div className="container">
+          <h2>"{rating} ⭐"</h2>
+        </div>
+        <div className="container">
+          <p>
+            {showMore ? description : description.substring(0, 240) + "..."}
+            <button onClick={() => setShowMore(!showMore)}>
+              {showMore ? "Show less" : "Show more"}
+            </button>
+          </p>
+        </div>
+        <div className="buttons container">
+          <button className="btn" onClick={previousSight}>
+            Previous
+          </button>
+          <button className="btn" onClick={nextSight}>
+            Next
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    id: 1,
+    name: "Notre-Dame Basilica of Montreal",
+    description:
+      "Notre-Dame Basilica is a basilica in the historic district of Old Montreal, in Montreal, Quebec, Canada. The interior of the church is amongst the most dramatic in the world and regarded as a masterpiece of Gothic Revival architecture. The vaults are coloured deep blue and decorated with golden stars, and the rest of the sanctuary is decorated in blues, azures, reds, purples, silver, and gold. It is filled with hundreds of intricate wooden carvings and several religious statues. Unusual for a church, the stained glass windows along the walls of the sanctuary do not depict biblical scenes, but rather scenes from the religious history of Montreal. Approximately 11 million people visit Notre-Dame Basilica every year, making it one of the most visited monuments in North America.",
+    rating: 4.7,
+    image:
+      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQS9yIsJJUg-wMeWzsR7p9eFWfvCaWf3_WAplf6HRXc8CBP7U9FsMgQ4bPnKBjKkf44wClf3IpAZOTvKPUTkXIY5A",
+  },
+  {
+    id: 2,
+    name: "The Montreal Museum of Fine Arts",
+    description:
+      "The Montreal Museum of Fine Arts is an art museum in Montreal, Quebec, Canada. It is the largest art museum in Canada by gallery space. The museum is located on the historic Golden Square Mile stretch of Sherbrooke Street. The MMFA is spread across five pavilions, and occupies a total floor area of 53,095 square metres, 13,000 of which are exhibition space. With the 2016 inauguration of the Michal and Renata Hornstein Pavilion for Peace, the museum campus was expected to become the eighteenth largest art museum in North America.",
+    rating: 4.7,
+    image:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTlCUXJIck2AfabeGmdKcuvXm2fefA4scsjs7vU3L6-iv3bgcmz0l-eydqoJev9j0TjHmSn17prQ5mT1-C8RiHddw",
+  },
+  {
+    id: 3,
+    name: "Mount Royal",
+    description:
+      "Mount Royal is a large intrusive rock hill or small mountain in the city of Montreal, immediately west of Downtown Montreal, Quebec, Canada. The best-known hypothesis for the origin of the name Montreal is that the name is taken from Mount Royal. The hill is part of the Monteregian Hills situated between the Laurentians and the Appalachian Mountains. It gave its Latin name, Mons Regius, to the Monteregian chain. The hill consists of three peaks: Colline de la Croix at 233 m, Colline d'Outremont at 211 m, and Westmount Summit at 201 m elevation above mean sea level.",
+    rating: 4.8,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB4wADdW_b4tiaW6jahSX0d2cw967jOaYD1HwExQYvZEwTAhGZ7VIqvEN2lBg8Klq07x3_tIRNNE9m37R56dTrlw",
+  },
+  {
+    id: 4,
+    name: "Montreal Botanical Garden",
+    description:
+      "The Montreal Botanical Garden is a large botanical garden in Montreal, Quebec, Canada comprising 75 hectares of thematic gardens and greenhouses. It was designated a National Historic Site of Canada in 2008 as it is considered to be one of the most important botanical gardens in the world due to the extent of its collections and facilities.",
+    rating: 4.6,
+    image:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSkdGpDisiDGW8iuST4N76VKv72Bhr4aRQcn1pmq0yXKo6wheQeEZ6UGN6bC35_YYMhm_HEcy6Rz_OTHf678FfpJA",
+  },
+  {
+    id: 5,
+    name: "Saint Joseph's Oratory of Mount Royal",
+    description:
+      "Saint Joseph's Oratory of Mount Royal is a Roman Catholic minor basilica and national shrine located at 3800 Queen Mary Road on Mount Royal's Westmount Summit in Montreal, Quebec. It is a National Historic Site of Canada and is Canada's largest church, with one of the largest church domes in the world. With its monumental scale, Renaissance Revival facade and contrasting Art Deco interior, the Oratory is recognizable not just in Montreal but around the world, attracting more than 2 million visitors and pilgrims to its steps each year. The Oratory is the highest building in Montreal, rising more than 30 meters above Mount Royal's summit.",
+    rating: 4.7,
+    image:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQajRI6G77bzXYrJnotlJXK5clS3fYUphiuAX8jg_QgXKK3btYazv1P1J7vdO1ZAX7gLAT3fi6MXw5efP6PoX-yEA",
+  },
+  {
+    id: 6,
+    name: "La Ronde",
+    description:
+      "La Ronde is an amusement park in Montreal, Quebec, Canada, built as the entertainment complex for Expo 67, the 1967 World Fair. Today, it is operated by Six Flags under an emphyteutic lease with the City of Montreal, which expires in 2065. It is the largest amusement park in Quebec and second largest in Canada. It is on 59 hectares located on the Northern tip of Saint Helen's Island. This is a man-made extension to the island in the space where the small Ronde Island had been. The park hosts L'International des Feux Loto-Québec, an international fireworks competition. La Ronde is one of three Six Flags parks not to be officially branded as a Six Flags park, Great Escape in Queensbury, New York, and Frontier City in Oklahoma City, Oklahoma being the other two.",
+    rating: 4.2,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL7ZcG2Y3VibPGvBCoZsNoZuwWf5ifpgzJfvT3m4OzoU5KfmAVUzjdZU-zJbJsb20uzwQbBOTIbrK8rTm3bdFryA",
+  },
+];
